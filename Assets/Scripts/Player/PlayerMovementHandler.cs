4d230b90c9b86933a47ab56a2638c2b6190179aa
Jan <jan.borwankar@gmail.com>
2026-05-19 20:24:51 +0200

    Lobby

---
+++ Assets/Scripts/Player/PlayerMovementHandler.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+namespace Friendslop 
+{
+    public class PlayerMovementHandler : NetworkBehaviour
+    {
+        void Update() 
+        {
+            if (isLocalPlayer) 
+            {
+                float h = Input.GetAxis("Horizontal");
+                float v = Input.GetAxis("Vertical");
+                Vector3 playerMovement = new Vector3(h * 0.25f, v * 0.25f, 0f);
+                transform.position = transform.position + playerMovement;
+            }
+        }
+    }
+}
+
